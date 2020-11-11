ddd5d0402746e08829045747b6b8258698621d41
Yash Mochi <yashkmochi007@gmail.com>
2020-11-11 22:24:17 +0530

    First and last commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [file, setFile] = useState();
+  const [response, setResponse] = useState('');
+  const inputRef = useRef();
+  const [loading, setLoading] = useState(false);
+  const fileChangeHandler = (e) => {
+    setResponse('');
+    setFile(e.target.files[0]);
+  };
+
+  const fileUploadHandler = (e) => {
+    const formData = new FormData();
+    formData.append("file", file, file.name);
+    setLoading(true);
+    setResponse('');
+    axios
+      .post("https://arcane-wave-10941.herokuapp.com/uploadFile", formData)
+      .then((res) => {
+        setResponse(res.data);
+        setLoading(false);
+        setFile(undefined);
+      }).catch(err => {
+        setResponse(err);
+        setLoading(false);
+        setFile(undefined);
+      });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        style={{ display: "none" }}
+        type="file"
+        onChange={fileChangeHandler}
+        ref={inputRef}
+        disabled={loading}
+      />
+      <button disabled={loading} onClick={() => inputRef.current.click()}>Pick file</button>
+      <br/>
+  <p>{file && file.name}</p>
+      <br/>
+      <button onClick={fileUploadHandler} disabled={Boolean(!file) || loading}>Upload</button>
+      <p>{loading ? 'Please wait...' : response}</p>
     </div>
   );
 }
